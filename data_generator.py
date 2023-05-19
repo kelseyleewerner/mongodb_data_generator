acafbeae12b1973f528df2e58b430abe94bcb054
kelseyleewerner <kelsey.lee.werner@gmail.com>
2023-05-19 15:26:22 -0700

    output data set to json file

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,6 @@
 from faker import Faker
-import pprint
+import json
+# import pprint
 import sys
 
 # Options for data_set_size are "test" or "demo"
@@ -22,5 +23,8 @@ for x in range(1, data_set_size):
         }
     data_set.append(item)
 
-pp = pprint.PrettyPrinter()
-pp.pprint(data_set)
+# pp = pprint.PrettyPrinter()
+# pp.pprint(data_set)
+
+with open(F"{data_set_option}_data.json", "w") as outfile:
+    json.dump(data_set, outfile)
