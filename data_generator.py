4f201ffd46893fea78178c1dab2db0370abfd3c0
kelseyleewerner <kelsey.lee.werner@gmail.com>
2023-05-19 15:09:56 -0700

    generate fake data

---
+++ data_generator.py
@@ -0,0 +1,26 @@
+from faker import Faker
+import pprint
+import sys
+
+# Options for data_set_size are "test" or "demo"
+data_set_option = sys.argv[1]
+data_set_size = int(sys.argv[2]) + 1
+
+fake = Faker()
+data_set = []
+
+for x in range(1, data_set_size):
+    if data_set_option == "test":
+        item = {
+            "count_test": x,
+            "color": fake.color_name()
+        }
+    else:
+        item = {
+            "count_demo": x,
+            "person": fake.name()
+        }
+    data_set.append(item)
+
+pp = pprint.PrettyPrinter()
+pp.pprint(data_set)
